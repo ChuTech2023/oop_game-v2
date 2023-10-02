bc71e4ad72bec24d5f15b95597b818c30dffa5aa
ChuTech2023 <alice.ntam@gmail.com>
2023-10-02 15:35:03 -0700

    step 1 of project

--- js/Phrase.js
+++ js/Phrase.js
@@ -1,3 +1,47 @@
 /* Treehouse FSJS Techdegree
  * Project 4 - OOP Game App
  * Phrase.js */
+
+//step 1 
+class Phrase {
+    constructor(phrase) {
+        this.phrase = phrase.toLowerCase();
+    }
+
+    addPhraseToDisplay() {
+        const phraseContainer = document.querySelector('#phrase ul');
+
+        let lis = "";
+        //loop to loop over phrase word
+        for (const char of this.phrase) {
+            if (char === " ") {
+                lis += '<li class="space"> </li>';
+            } else {
+                // create a class with hidden class and the character
+                lis += ` <li class="hide letter ${char}">${char}</li>`;
+            }
+        }
+        phraseContainer.innerHTML = lis;
+    }
+
+    checkLetter(letter) {
+        if (this.phrase.includes(letter)) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    showMatchedLetter(letter) {
+        const letters = document.querySelectorAll('.hide'); 
+
+        //loop to see which li contained a certain letter
+        letters.forEach(li => {
+            if (li.innerText === letter) {
+                li.classList.add('show');
+                li.classList.remove('hide');
+            }
+        });
+    }
+}
+
