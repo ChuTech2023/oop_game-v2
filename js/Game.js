b3270ad3306037e46bb0e9c8f3a75344512bc566
ChuTech2023 <alice.ntam@gmail.com>
2023-10-06 11:00:45 -0700

    completed project

--- js/Game.js
+++ js/Game.js
@@ -18,19 +18,22 @@ class Game {
         
         this.activePhrase = null;
     }
+    //hiding the overlay screen , then generate a random phrase and display phrase to the page
     startGame() {
-       const overlay = document.querySelector('.overlay');
+       const overlay = document.querySelector('#overlay');
        overlay.style.display = 'none';
        this.activePhrase = this.getRandomPhrase();
        this.activePhrase.addPhraseToDisplay();
     }
 
+    //retrives a phrase randomly
     getRandomPhrase() {
         //created variable to retrive one of the phrases at random
         const randomIndex = Math.floor(Math.random() * this.phrases.length);
         return this.phrases[randomIndex];
     }
 
+    //matches chosen letter in each phrase and check if I won
     handleInteraction (button) {
         //created variable to retrive the text on each button 
       const guessLetter = button.innerText;
@@ -38,7 +41,7 @@ class Game {
         button.classList.add('chosen');
         this.activePhrase.showMatchedLetter(guessLetter);
         if (this.checkForWin()) {
-            this.gameOver();
+            this.gameOver(true);
         }
 
       } else {
@@ -47,17 +50,19 @@ class Game {
       }
     }
 
+    //removes life each time a wrong character is chosen and the missed property incremenated
     removeLife () {
         //select all the liveheart img
-       const hearts = document.querySelectorAll('.tries');
+       const hearts = document.querySelectorAll('.tries img');
        this.missed++
-       if (this.misses > 5) {
-            this.gameOver();
+       if (this.missed >= 5) {
+            this.gameOver(false);
        } else {
         hearts[this.missed - 1].src = 'images/lostHeart.png';
        }
     }
 
+    //ckecks if all the letters are revealed
     checkForWin () {
         const phraseLetters = document.querySelectorAll('.hide');
         if (phraseLetters.length <= 0) {
@@ -67,19 +72,36 @@ class Game {
         }
     }
 
+    //ends the game and the display a win/loss message, and resets the game
     gameOver (outcome) {
-        const overlay = document.querySelector('.overlay');
+        const overlay = document.querySelector('#overlay');
         const gameOverMessage = document.querySelector('#game-over-message');
+        overlay.style.display = 'block';
+        overlay.classList.remove('start');
         if (outcome) {
-           overlay.style.display = 'block';
            overlay.classList.add('win');
-           overlay.classList.remove('start');
            gameOverMessage.innerText = 'You are a winner!';
         } else {
-            overlay.style.display = 'block';
            overlay.classList.add('lose');
-           overlay.classList.remove('start');
            gameOverMessage.innerText = 'Try Again!';
         }
+        this.resetGame();
+    }
+
+    //step 4, reset the game by resetting missed to 0 
+    resetGame () {
+        document.querySelector('#phrase ul').innerHTML = '';
+        const qwertyBtns = document.querySelectorAll('#qwerty button');
+        qwertyBtns.forEach(btn => {
+            btn.className = 'key';
+            btn.disabled = false;
+        });
+
+        this.missed = 0;
+        const hearts = document.querySelectorAll('.tries img');
+        hearts.forEach(ht => {
+            ht.src = 'images/liveHeart.png';
+        })
+        
     }
 }
--- js/app.js
+++ js/app.js
@@ -1,3 +1,22 @@
 /* Treehouse FSJS Techdegree
  * Project 4 - OOP Game App
  * app.js */
+
+//step 3
+
+const startBtn = document.querySelector('#btn__reset');
+
+//instance of  game
+const game = new Game();
+
+//start the game 
+startBtn.addEventListener('click', () => {
+    game.startGame();
+})
+
+//add a click event to keyboard buttons to handle interactions
+document.querySelector('#qwerty').addEventListener('click', (event) =>{
+    if (event.target.className === 'key') {
+      game.handleInteraction(event.target);  
+    }
+})
